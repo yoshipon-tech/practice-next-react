ba469036e466af98806b93e174caf3f35b3c9b97
yoshipon-tech <yoshipon.tech@gmail.com>
2024-10-25 05:52:05 +0900

    P164まで

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,11 +7,23 @@ import {
   TwoColumnMain,
   TwoColumnSidebar,
 } from "components/TwoColumn";
+import Image from "next/image";
+import eyecatch from "images/about.jpg";
 
 export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width: 1152px) 1152px, 100vw"
+          priority
+          placeholder="blur"
+        />
+      </figure>
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
