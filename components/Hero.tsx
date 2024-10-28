3615f16136186c6762350fec21549a5838864365
yoshipon-tech <yoshipon.tech@gmail.com>
2024-10-28 15:35:45 +0900

    追加修正3

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,6 +22,8 @@ export default function Hero({ title, subtitle, imageOn = false }: IProps) {
             alt=""
             layout="responsive"
             sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
+            priority
+            placeholder="blur"
           />
         </figure>
       )}
