cf2a5fe9ec4abc334b9909ee0dc6c762137682b6
yoshipon-tech <yoshipon.tech@gmail.com>
2024-11-15 23:56:31 +0900

    Blog一覧追加

---
+++ components/Posts.tsx
@@ -0,0 +1,45 @@
+import cssModule from "src/styles/Posts.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+type Post = {
+  title: string;
+  slug: string;
+  eyecatch: Eyecatch;
+};
+
+type Eyecatch = {
+  url: string;
+  width: number;
+  height: number;
+  blurDataURL: string;
+};
+
+export type Posts = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: Posts) {
+  return (
+    <div className={cssModule.gridContainer}>
+      {posts.map(({ title, slug, eyecatch }) => (
+        <article className={cssModule.post} key={slug}>
+          <Link href={`/blog/${slug}`}>
+            <figure>
+              <Image
+                src={eyecatch.url}
+                alt=""
+                layout="fill"
+                objectFit="cover"
+                sizes="(min-width: 1152px) 576px, 50vw"
+                placeholder="blur"
+                blurDataURL={eyecatch.blurDataURL}
+              />
+            </figure>
+            <h2>{title}</h2>
+          </Link>
+        </article>
+      ))}
+    </div>
+  );
+}
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,12 +1,32 @@
 import Container from "components/Container";
 import Hero from "components/Hero";
 import Meta from "components/Meta";
+import Posts from "components/Posts";
+import type { Posts as PostsType } from "components/Posts";
+import { getAllPosts } from "lib/api";
+import { eyecatchLocal } from "lib/constants";
+import { getPlaiceholder } from "plaiceholder";
 
-export default function Home() {
+export default function Home({ posts }: PostsType) {
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
       <Hero title="Blog" subtitle="Recent Posts" />
+      <Posts posts={posts} />
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty("eyecatch")) {
+      post.eyecatch = eyecatchLocal;
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    post.eyecatch.blurDataURL = base64;
+  }
+
+  return { props: { posts } };
 }
